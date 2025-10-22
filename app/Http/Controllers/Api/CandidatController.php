20f7ed471b1a608ee771e85dda293583141b8b5e
GADJI Kangni Arnaud <arnaudgadji675@gmail.com>
2025-10-22 10:45:06 +0000

    Création des controlleurs

--- app/Http/Controllers/Api/CandidatController.php
+++ app/Http/Controllers/Api/CandidatController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Candidat;
 use Illuminate\Http\Request;
+use PhpParser\Node\Stmt\TryCatch;
 
 class CandidatController extends Controller
 {
@@ -12,7 +14,20 @@ class CandidatController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $candidats = Candidat::all();
+            return response()->json([
+                'success' => true,
+                'data' => $candidats
+            ], 200);
+          
+
+        } catch (\Throwable $th) {
+            return response()->json([
+               'success' => false,
+               'message' => 'Erreur lors de la récupération des candidats'  . $th->getMessage(),
+            ], 400);
+        }
     }
 
     /**
@@ -28,7 +43,32 @@ class CandidatController extends Controller
      */
     public function store(Request $request)
     {
-        //
+       try {
+           $data = $request->validate([
+            "lastName" => "required|string|min:3",
+            "firstName" => "required|string|min:3",
+            "nationality" => "required|string",
+            "age" => "required|integer|min:18",
+            "weight" => "nullable",
+            "height" => "nullable",
+            "shortDescription" => "nullable",
+            "fullDescription" => "nullable",
+            "profilePhoto" => "nullable",
+           ]);
+
+           $candidat = Candidat::create($data);
+           return response()->json([
+            'success' => true,
+            'message' => 'Candidat crée avec succès',
+            'data' => $candidat,
+           ], 200);
+
+       } catch (\Throwable $th) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Erreur lors de la création du candidat ',
+        ], 400);
+       }
     }
 
     /**
@@ -36,7 +76,18 @@ class CandidatController extends Controller
      */
     public function show(string $id)
     {
-        //
+        try {
+            $candidat = Candidat::findOrFail($id);
+            return response()->json([
+                'success' => true,
+                'data' => $candidat,
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Candidat non trouvé' .$th->getMessage(),
+            ], 400);
+        }
     }
 
     /**
@@ -52,7 +103,32 @@ class CandidatController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+         try {
+           $data = $request->validate([
+            "lastName" => "required|string|min:3",
+            "firstName" => "required|string|min:3",
+            "nationality" => "required|string",
+            "age" => "required|integer|min:18",
+            "weight" => "nullable",
+            "height" => "nullable",
+            "shortDescription" => "nullable",
+            "fullDescription" => "nullable",
+            "profilePhoto" => "nullable",
+           ]);
+
+           $candidat = Candidat::findOrFail($id)->update($data);
+           return response()->json([
+            'success' => true,
+            'message' => 'Candidat modifié avec succès',
+            'data' => $candidat,
+           ], 200);
+
+       } catch (\Throwable $th) {
+        return response()->json([
+            'success' => false,
+            'message' => 'Erreur lors de la modification du candidat ' . $th->getMessage(),
+        ], 400);
+       }
     }
 
     /**
@@ -60,6 +136,16 @@ class CandidatController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            Candidat::findOrFail($id)->delete();
+            return response()->json([
+                'success' => true,
+                'message' => 'Candidat supprimé avec succès',
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Erreur lors de la suppression' .$th->getMessage(),
+            ]);
+        }
     }
 }
